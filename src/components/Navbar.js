38fe086d5641b3115a9c165cf23fb4e394223d27
stephenmphughes <stephenmphughes@gmail.com>
2025-04-02 22:32:35 +0100

    Styled Navbar and updated global styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,17 @@
-// src/components/Navbar.js
 import React from "react";
+import "./Navbar.css"; // We'll define styles here
 
-function Navbar() {
+const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
-      <div className="container">
-        <a className="navbar-brand" href="/">Clothing Store</a>
-      </div>
+    <nav className="navbar-container">
+      <div className="navbar-brand">ClothingStore</div>
+      <ul className="navbar-links">
+        <li><a href="/">Home</a></li>
+        <li><a href="#products">Products</a></li>
+        <li><a href="#about">About</a></li>
+      </ul>
     </nav>
   );
-}
+};
 
 export default Navbar;
